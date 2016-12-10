2cbc3a3f8bdb4cefee5c027d8ad716124094ee31
shichiyou <shichiyou@outlook.com>
2016-12-10 14:35:01 +0900

    Workspace init. Mobile Center Enabled.

--- SwiftApp/AppDelegate.swift
+++ SwiftApp/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import MobileCenter
+import MobileCenterAnalytics
+import MobileCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        MSMobileCenter.start("e50720c3-0436-4335-8fdc-bd73f4900cd0", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
+        
         return true
     }
 
