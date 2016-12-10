5d4f2b45012f9ac38ca625edaa8ddfdd015ef8ac
shichiyou <shichiyou@outlook.com>
2016-12-10 14:30:04 +0900

    Initial Commit

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Tanaka Yasunobu on 2016/12/10.
+//  Copyright © 2016年 Tanaka Yasunobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
